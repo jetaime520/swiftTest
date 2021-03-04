e797d66c1ba7218ee6503ab0b7b9340c78477a26
Shin <jetaime1201@gmail.com>
2021-03-04 20:37:29 +0800

    Initial Commit

---
+++ swiftTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftTest
+//
+//  Created by Shin Shen on 2021/3/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
